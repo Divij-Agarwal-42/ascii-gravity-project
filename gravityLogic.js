673522e1d8f09f3b9ec55427a972a79528d37d90
Sandilya Kambhampati <kprsandilya@gmail.com>
2023-09-16 11:30:37 -0400

    Updated Canvas

--- gravityLogic.js
+++ gravityLogic.js
@@ -1,8 +1,11 @@
 //This is the gravity logic for the website
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 document.getElementById("LoginButton").addEventListener("click", function() {
-    if (document.getElementById("UserNameInput").value === user && document.getElementById("PasswordInput").value === password) {
-
-        alert("Success!");
-
-    }
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 150, 75);
+});
+document.getElementById("ClearButton").addEventListener("click", function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
