f50b576c22eed656889ab252692b6b15e7ad7812
Sandilya Kambhampati <kprsandilya@gmail.com>
2023-09-16 12:24:03 -0400

    asdf

--- gravityLogic.js
+++ gravityLogic.js
@@ -4,11 +4,18 @@ const ctx = canvas.getContext("2d");
 
 document.getElementById("DrawButton").addEventListener("click", function() {
     let userInput = document.getElementById("TextInput").value;
-    console.log(userInput);
-    console.log("asdf");
     ctx.font = "30px Arial";
     ctx.fillText(userInput, 10, 50);
 });
 document.getElementById("ClearButton").addEventListener("click", function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
+document.getElementById("AnimateButton").addEventListener("click", function() {
+    let userInput = document.getElementById("TextInput").value;
+    ctx.font = "30px Arial";
+    window.requestAnimationFrame(draw);
+});
+
+function draw{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
