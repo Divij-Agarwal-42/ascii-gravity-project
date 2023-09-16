61c594da0b38e901336931789743ec5b475c19af
Sandilya Kambhampati <kprsandilya@gmail.com>
2023-09-16 11:17:38 -0400

    This is the gravity logic.

---
+++ gravityLogic.js
@@ -0,0 +1,8 @@
+//This is the gravity logic for the website
+document.getElementById("LoginButton").addEventListener("click", function() {
+    if (document.getElementById("UserNameInput").value === user && document.getElementById("PasswordInput").value === password) {
+
+        alert("Success!");
+
+    }
+});
